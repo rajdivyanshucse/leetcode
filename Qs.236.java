92b50475488a61106fe3b70e02df3d8ad00c4328
rajdivyanshucse <rajdivyanshu.cse@gmail.com>
2026-07-18 07:07:34 +0530

    Qs no. 236 approach 2 of LCA

--- Qs.236.java
+++ Qs.236.java
@@ -32,4 +32,25 @@ class Solution {
         path.remove(path.size()-1);
         return false;
     }
+
+
+    // Approach 2 of lca
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q){
+            return root;
+        }
+
+        TreeNode leftLca = lowestCommonAncestor(root.left ,p ,q); 
+        TreeNode rightLca = lowestCommonAncestor(root.right ,p ,q);
+
+        if(rightLca == null){
+            return leftLca;
+        } 
+        if(leftLca == null){
+            return rightLca;
+        } 
+
+        return root;
+    }
 }
